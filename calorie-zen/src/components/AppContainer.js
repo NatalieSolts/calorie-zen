807414e0874e2ed996c9ea8235a9a397046a46e6
Наталья <mrs.solts@mail.ru>
2023-02-21 15:45:26 +0300

    feat: добавить заготовку для двух колонок

--- calorie-zen/src/components/AppContainer.js
+++ calorie-zen/src/components/AppContainer.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function AppContainer() {
   return (
-    <p>Потреблённые калории:</p>
+    <div className="container">
+      <div className="calories">
+        <h2>Калории:</h2>
+      </div>
+      <h2>Добавьте продукты</h2>
+    </div>
   );
 }
-
-export default AppContainer;
