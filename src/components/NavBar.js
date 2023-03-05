5e68f7455dacc3076afbf2734a81f2852f13f8d4
Наталья <mrs.solts@mail.ru>
2023-03-05 23:48:27 +0300

    refactor: создать ссылки, используя компоненты  NavLink, а не теги <a>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom'
 import './NavBar.css';
 
 function NavBar() {
-  return (
-    <nav className="menu">
-      <a className="menu__item menu__item_active" href="#">Домой</a>
-      <a className="menu__item" href="#">Советы</a>
-    </nav>
-  );
-}
-
-export default NavBar;
+    return (
+        <nav className="menu">
+          <NavLink to="/" className={({isActive}) => `menu__item ${isActive ? "menu__item_active" : ""}`}>Домой</NavLink>
+          <NavLink to="/tips" className={({isActive}) => `menu__item ${isActive ? "menu__item_active" : ""}`}>Советы</NavLink>
+        </nav>
+      );
+    }
+    
+    export default NavBar;
