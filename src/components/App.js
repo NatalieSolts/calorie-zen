b663737eaa51093f4eb000257252ab492710bd9c
Наталья <mrs.solts@mail.ru>
2023-03-05 23:45:43 +0300

    feat: добавить импорты компонентов в App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import AppContainer from './AppContainer';
+import NavBar from './NavBar';
+import Diary from './Diary';
+import Tips from './Tips';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <AppContainer />      
-    </>
+      <main className="content">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Diary />} />
+          <Route path="/tips" element={<Tips />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
