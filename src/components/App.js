c639ab2c562e1f49c33eb556f191f1d05ff4af16
Наталья <mrs.solts@mail.ru>
2023-03-26 22:04:38 +0300

    feat: настроить переадресацию пользователя

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import NavBar from './NavBar';
-import Diary from './Diary';
-import Tips from './Tips';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Header from "./Header";
+import Diary from "./Diary";
+import Tips from "./Tips";
+import Register from "./Register";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import "./styles/App.css";
 
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <main className="content">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Diary />} />
-          <Route path="/tips" element={<Tips />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    };
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <main className="content">
+          {this.state.loggedIn && <NavBar />}
+          <Routes>
+            <Route
+              path="/"
+              element={
+                this.state.loggedIn ? (
+                  <Navigate to="/diary" replace />
+                ) : (
+                  <Navigate to="/login" replace />
+                )
+              }
+            />
+            <Route path="/diary" element={<Diary />} />
+            <Route path="/tips" element={<Tips />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
