1019104d62f0b4f1816642abffea08ed4f83f499
Наталья <mrs.solts@mail.ru>
2023-03-26 21:45:08 +0300

    feat: добавить ссылки на компоненты Register и Login в компонент NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom'
-import './NavBar.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
-    return (
-        <nav className="menu">
-          <NavLink to="/" className={({isActive}) => `menu__item ${isActive ? "menu__item_active" : ""}`}>Домой</NavLink>
-          <NavLink to="/tips" className={({isActive}) => `menu__item ${isActive ? "menu__item_active" : ""}`}>Советы</NavLink>
-        </nav>
-      );
-    }
-    
-    export default NavBar;
+  return (
+    <nav className="menu">
+      <NavLink
+        to="/diary"
+        className={({ isActive }) =>
+          `menu__item ${isActive ? "menu__item_active" : ""}`
+        }
+      >
+        Домой
+      </NavLink>
+      <NavLink
+        to="/tips"
+        className={({ isActive }) =>
+          `menu__item ${isActive ? "menu__item_active" : ""}`
+        }
+      >
+        Советы
+      </NavLink>
+    </nav>
+  );
+}
+
+export default NavBar;
